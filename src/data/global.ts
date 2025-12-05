47e6fb16c6a3131b48cc70560ea48ca2c3a7494b
aPRINTure <info@aprinture.com>
2025-12-05 04:13:07 +0100

    About Me Almost Done

--- src/data/global.ts
+++ src/data/global.ts
@@ -38,8 +38,103 @@ export const contactInfo = {
     email: "info@filipsteficar.com",
     address: "Jastrebarsko, Croatia"
 }
-// Social Media
-export const socialMedia = {
-    instagram: "https://www.instagram.com/filip.steficar/",
-    unsplash: "https://unsplash.com/@filipsteficar"
+// Define the SocialMediaItem Interface
+interface SocialMediaItem {
+    label: string
+    href: string
 }
+// Social Media
+export const socialMedia: SocialMediaItem[] = [
+    {
+        label: "Instagram",
+        href: "https://www.instagram.com/filip.steficar"
+    },
+    {
+        label: "Unsplash",
+        href: "https://unsplash.com/@filipsteficar"
+    },
+    {
+        label: "Linkedin",
+        href: "https://www.linkedin.com/in/filip-steficar-6b0b65283",
+    }
+]
+// Define the Toolbox Interface
+interface Toolbox {
+    label: string
+    href: string
+}
+// Toolbox
+export const myToolbox: Toolbox[] = [
+    {
+        label: "Davinci Resolve",
+        href: "/"
+    },
+    {
+        label: "FL Studio",
+        href: "/"
+    },
+    {
+        label: "WordPress",
+        href: "/",
+    },
+    {
+        label: "Astro JS",
+        href: "/",
+    },
+    {
+        label: "n8n",
+        href: "/",
+    },
+    {
+        label: "Darktable",
+        href: "/",
+    },
+    {
+        label: "Affinity",
+        href: "/",
+    }
+]
+// Define the SeenOn Interface
+interface SeenOn {
+    label: string
+    href: string
+}
+// Seen On
+export const seenOn: SeenOn[] = [
+    {
+        label: "Wix",
+        href: "/"
+    },
+    {
+        label: "Squarespace",
+        href: "/"
+    },
+    {
+        label: "WordPress",
+        href: "/",
+    },
+    {
+        label: "PicsArt",
+        href: "/",
+    },
+    {
+        label: "Unfold",
+        href: "/",
+    },
+    {
+        label: "Figma",
+        href: "/",
+    },
+    {
+        label: "Notion",
+        href: "/",
+    },
+    {
+        label: "Medium",
+        href: "/",
+    },
+    {
+        label: "Trello",
+        href: "/",
+    }
+]
