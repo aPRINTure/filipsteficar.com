496cc53e25411495cda05101c4c828becc70d657
aPRINTure <info@aprinture.com>
2025-12-07 03:26:19 +0100

    Nginx Config

--- src/data/global.ts
+++ src/data/global.ts
@@ -58,50 +58,6 @@ export const socialMedia: SocialMediaItem[] = [
         href: "https://www.linkedin.com/in/filip-steficar-6b0b65283",
     }
 ]
-// Define the Toolbox Interface
-interface Toolbox {
-    label: string
-    href: string
-}
-// Toolbox
-export const myToolbox: Toolbox[] = [
-    {
-        label: "DaVinci Resolve",
-        href: "https://www.blackmagicdesign.com/products/davinciresolve"
-    },
-    {
-        label: "FL Studio",
-        href: "https://www.image-line.com/"
-    },
-    {
-        label: "WordPress",
-        href: "https://wordpress.org/",
-    },
-    {
-        label: "Astro JS",
-        href: "https://astro.build/",
-    },
-    {
-        label: "n8n",
-        href: "https://n8n.io/",
-    },
-    {
-        label: "Darktable",
-        href: "https://www.darktable.org/",
-    },
-    {
-        label: "Affinity",
-        href: "https://www.affinity.studio/",
-    },
-    {
-        label: "Bricks Builder",
-        href: "https://bricksbuilder.io/",
-    },
-    {
-        label: "Tailwind CSS",
-        href: "https://tailwindcss.com/",
-    }
-]
 // Define the SeenOn Interface
 interface SeenOn {
     label: string
